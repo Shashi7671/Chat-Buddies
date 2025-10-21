f2ed5c0484b33489eda4fca77d83c7648ac3fa6b
Borra Shashi Ram <shashiramgoud16@gmail.com>
2025-10-21 20:36:10 +0530

    server.js updated for vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import { AuthContext } from '../context/AuthContext'
 const App = () => {
   const { authUser } = useContext(AuthContext)
   return (
-    <div className="bg-[url('./src/assets/back.png')] bg-contain">
+    <div className="bg-[url('/back.png')] bg-contain">
       <Toaster/>
       <Routes>
         <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
